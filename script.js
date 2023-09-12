3742729454e14bf8b942dcb2aef07ce336eac8f8
SlvGbn <permkenny@gmail.com>
2023-09-12 14:34:17 +0500

    finish main part of the game

--- script.js
+++ script.js
@@ -9,27 +9,78 @@ function getRandomInt(max) {
 }
 
 const elIndex = getRandomInt(words.length);
-const answerIndex = getRandomInt(words[elIndex].length);
 const word = words[elIndex];
+const answerIndex = getRandomInt(word.length);
+let tryCount = 3;
 
 console.log(
     word, 
     word[answerIndex], 
     answerIndex
 );
+function createRepeatButton() {
+    const aEl = document.createElement("a");
+    aEl.href = "javascript:window.location.reload(true)";
+    aEl.textContent = "Начать заного";
+    return aEl;
+}
 
+function handleTryDecrease() {
+    if (tryCount == 0) {
+        const infoEl = document.querySelector("#info");
+
+        const messageEl = document.createElement("h2");
+        messageEl.textContent = "Попытки исчерпаны!";
+        infoEl.appendChild(messageEl);
+
+        const answerEl = document.createElement("h2");
+        answerEl.textContent = "Правильная ответ: \"" + word[answerIndex] + "\" по счету " + (answerIndex + 1);
+        infoEl.appendChild(answerEl);
+
+        infoEl.appendChild(createRepeatButton());
+    }
+}
+
+function handleWin() {
+    console.log("verno");
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+        button.style.backgroundColor = "blue";
+    })
+
+    const infoEl = document.querySelector("#info");
+
+    const messageEl = document.createElement("h2");
+    messageEl.textContent = "Вы угадали!";
+    infoEl.appendChild(messageEl);
+
+    infoEl.appendChild(createRepeatButton());
+}
+
+function handleClick(button, buttonIndex) {
+    console.log("sad");
+    if (buttonIndex == answerIndex) {
+        handleWin();
+    }
+    else {
+        button.style.backgroundColor = "red";
+        tryCount--;
+        setTimeout(() => { 
+            button.style.backgroundColor = "beige";
+        }, 2000); 
+        handleTryDecrease();
+    }
+}
 
 window.addEventListener("load", () => {
     const contEl = document.querySelector("#container");
 
-    word.split("").forEach((v) => {
-        console.log(v);
+    word.split("").forEach((v, i) => {
         const button = document.createElement("button");
         button.textContent = v;
+        button.addEventListener("click", () => {
+            handleClick(button, i);
+        })
         contEl.appendChild(button);
     });
-
-
-    
-    
 })
